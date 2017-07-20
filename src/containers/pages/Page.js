ea24fead40c20594ca252858263cece87904d726
Xiaofei <xifzhang@thoughtworks.com>
2017-07-20 22:15:29 +0800

    handle component lifecycle to support browser reload

--- src/containers/pages/Page.js
+++ src/containers/pages/Page.js
@@ -9,30 +9,35 @@ class Page extends React.Component {
             content: {}
         };
     }
-    componentDidMount() {
-        const {article, type} = this.props;
-        const articles = article[type];
-        fetch(articles[0].url, {method: "GET"})
-            .then((response) => {
-                return response.text();
-            })
-            .then(content => {
-                console.log(content)
-                this.setState({content})
-            })
+    componentWillReceiveProps(nextProps) {
+        const {article = {}, type} = nextProps;
+        const articles = article[type] || [];
+        if (articles.length > 0) {
+            fetch(articles[0].url, {method: "GET"})
+                .then((response) => {
+                    return response.text();
+                })
+                .then(content => {
+                    console.log(content)
+                    this.setState({content})
+                })
+        }
     }
     render() {
-        const {type, article} = this.props;
-        const articles = article[type];
-        return (
-            <div className={type}>
-                <div className="left-menu">
-                    <div className="mask"></div>
-                    <div className="menu">{articles[0].title}</div>
+        const {type, article ={}} = this.props;
+        const articles = article[type] || [];
+        if (articles.length > 0) {
+            return (
+                <div className={type}>
+                    <div className="left-menu">
+                        <div className="mask"></div>
+                        <div className="menu">{articles[0].title}</div>
+                    </div>
+                    <div className="content" dangerouslySetInnerHTML={{__html: this.state.content}}></div>
                 </div>
-                <div className="content" dangerouslySetInnerHTML={{__html: this.state.content}}></div>
-            </div>
-        )
+            )
+        }
+        return null;
     }
 }
 
