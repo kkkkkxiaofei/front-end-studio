ea061fb036af8f0d61de05e4e3fcc8e4e342e209
Xiaofei <xifzhang@thoughtworks.com>
2017-07-24 17:33:39 +0800

    simplify state data

--- src/reducers/articleReducer.js
+++ src/reducers/articleReducer.js
@@ -2,7 +2,7 @@ import {ARTICLE_FETCH}  from '../actions/article'
 
 export default function(state = {}, action) {
     if(action.type === ARTICLE_FETCH) {
-        return Object.assign({}, state, JSON.parse(action.data))
+        return JSON.parse(action.data);
     }
-    return state
+    return state;
 }
