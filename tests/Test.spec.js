85f48c380b9d7377ba25c21c205c4055f67112bf
Xiaofei <xifzhang@thoughtworks.com>
2017-08-02 13:40:35 +0800

    add test framework

---
+++ tests/Test.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Test from "../src/containers/pages/Test";
+
+describe('<Test/>', function() {
+    it('should render title', function() {
+        const wrapper = shallow(<Test />);
+        expect(wrapper.find('.content').text()).to.equal('This is component for test.');
+    });
+});
